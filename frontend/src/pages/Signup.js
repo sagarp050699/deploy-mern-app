1d2eb3cb0153ea60ef8b499bbf88763a5ae46f27
Sagar Patil <121754768+sagarp050699@users.noreply.github.com>
2024-09-09 22:33:32 +0530

    Update Signup.js

--- frontend/src/pages/Signup.js
+++ frontend/src/pages/Signup.js
@@ -27,7 +27,7 @@ function Signup() {
             return handleError('name, email and password are required')
         }
         try {
-            const url = `http://localhost:8080/auth/signup`;
+            const url = `https://deploy-mern-app-api-gamma.vercel.app/auth/signup`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
