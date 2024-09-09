78f00697e87e0f9f0e00039311c9a3ee6ab5a8c2
sagar <sagarp050699@gmail.com>
2024-09-09 22:55:57 +0530

    ok

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,7 @@ function Login() {
             return handleError('email and password are required')
         }
         try {
-            const url = https://deploy-mern-app-api-gamma.vercel.app/auth/login`;
+            const url = `https://https://deploy-mern-app-api-gamma.vercel.app/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
